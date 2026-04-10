7349da56b507d1b84114a9924c71de94386078b9
Rdz2906 <ihernandez@dragontec.net>
2026-04-10 11:20:10 -0600

    feat: implementacion de recuperacion de contraseña

--- src/models/Usuario.ts
+++ src/models/Usuario.ts
@@ -1,9 +1,28 @@
 import { Schema, model } from "mongoose";
 
 const UsuarioSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  resetPasswordToken: {
+    type: String,
+    default: null,
+  },
+  resetPasswordExpires: {
+    type: Date,
+    default: null,
+  },
 });
 
 export default model("Usuario", UsuarioSchema);
